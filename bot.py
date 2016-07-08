6076f0b9d9952f05e1e854c23602ef4885b66f3a
hyliu <hyliu@linkedin.com>
2016-07-08 00:04:17 -0700

    add fb messaging webhook verification code

--- bot.py
+++ bot.py
@@ -3,9 +3,14 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route("/bot", methods=['POST'])
-def chat():
-    return "Hello human! \n"
+VALIDATION_TOKEN = 'aijiajia'
 
+@app.route("/bot", methods=['GET','POST'])
+def chat():
+    if request.method == 'GET' and request.args.get('verify_token'):
+        return request.args.get('challenge')
+    else:
+        return "Hello human! \n"
+        
 if __name__ == "__main__":
     app.run()
