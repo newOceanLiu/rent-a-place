2d943cc4664bda412ebec5b1f25801b262cdc384
hyliu <hyliu@linkedin.com>
2016-07-08 00:20:53 -0700

    improve verification logic

--- bot.py
+++ bot.py
@@ -7,10 +7,17 @@ VALIDATION_TOKEN = 'aijiajia'
 
 @app.route("/bot", methods=['GET','POST'])
 def chat():
-    if request.method == 'GET' and request.args.get('verify_token'):
-        return request.args.get('challenge')
+    is_verification, verification_response = handle_verification()
+    if is_verification:
+        return verification_response
     else:
         return "Hello human! \n"
-        
+
+def handle_verification():
+    if request.method == 'GET' \
+        and request.args.get('hub.mode') == 'subscribe' \
+        and request.args.get('hub.verify_token') == VALIDATION_TOKEN:
+        return True, request.args.get('hub.challenge')
+    return False, None
 if __name__ == "__main__":
     app.run()
